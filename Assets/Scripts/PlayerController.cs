9762d5e12c3f62cd0512863164a5d3bfcc8066ed
Lynn Barlow <lynn.b.barlow3@gmail.com>
2019-09-23 16:40:33 -0600

    created new repo, moved unity lesson into new repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 15f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Adds inputs for vehicle movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Moves vehicle forward and horizontal
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);  
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+     }
+}
